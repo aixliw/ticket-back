254b1e1e84830e67c5e2d43f2c65d711359cd263
菜-_-菜 <11098417+vegetables---food@user.noreply.gitee.com>
2023-02-24 09:52:02 +0800

    add ticket  Buyday， edit ticket message

---
+++ src/main/java/com/hnust/wx_ticket/Vo/TicketDay.java
@@ -0,0 +1,8 @@
+package com.hnust.wx_ticket.Vo;
+
+import lombok.Data;
+
+@Data
+public class TicketDay {
+    private String buyDay;
+}
--- src/main/java/com/hnust/wx_ticket/controller/TicketController.java
+++ src/main/java/com/hnust/wx_ticket/controller/TicketController.java
@@ -1,6 +1,8 @@
 package com.hnust.wx_ticket.controller;
+
 import cn.hutool.core.bean.BeanUtil;
 import com.hnust.wx_ticket.Utils.R;
+import com.hnust.wx_ticket.Vo.TicketDay;
 import com.hnust.wx_ticket.Vo.TicketVo;
 import com.hnust.wx_ticket.entity.Ticket;
 import com.hnust.wx_ticket.service.TicketService;
@@ -11,10 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author perfect imitator
@@ -31,13 +32,24 @@ public class TicketController {
     //获取用于展示到电影票上的信息
     @GetMapping(produces = "application/json")
     public R getTicketInfo(@RequestParam Integer ticketId,
-                           @RequestParam Integer filmId){
+                           @RequestParam Integer filmId) {
         Ticket ticket = ticketService.getById(ticketId);
         int movieId = ticket.getMovieId();
-        TicketVo ticketVo = ticketService.getTicket(ticketId,movieId,filmId);
-        BeanUtil.copyProperties(ticket,ticketVo);
+        TicketVo ticketVo = ticketService.getTicket(ticketId, movieId, filmId);
+        BeanUtil.copyProperties(ticket, ticketVo);
         System.out.println(ticketVo.toString());
-        return R.ok().data("ticketVo",ticketVo);
+        return R.ok().data("ticketVo", ticketVo);
+
+    }
+
+    //获取用于展示到电影票上的信息
+    @GetMapping(value = "/buyday",produces= "application/json")
+    public R getTicketInfo(@RequestParam Integer ticketId) {
+        Ticket ticket = ticketService.getById(ticketId);
+        TicketDay ticketDay = new TicketDay();
+        BeanUtil.copyProperties(ticket, ticketDay);
+        return R.ok().data("ticketDay", ticketDay);
+
     }
 
 }
